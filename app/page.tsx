643f4f910161a9fffbdb46a8195fdc821c0411db
Victor Zerefos <101561895+WebZerefos@users.noreply.github.com>
2022-12-21 09:02:25 -0300

    Frontend and Backend SetUp

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+	return (
+		<div>
+			<h1 className='text-4xl'>Welcome to the Blog.</h1>
+			<p>Some content..</p>
+		</div>
+	)
+}
+
+export default Home
